8d91709696c34ec43f0d0145128189e9c7f02961
afharyo <afharyodiwseno@gmail.com>
2026-09-24 22:07:09 +0700

    remove some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import { useState } from "react";
-import heroImg from "./assets/hero.png";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
 import { Header } from "./layout/Header";
 import HeroSection from "./layout/HeroSection";
 import PetaSection from "./layout/PetaSection";
-import RatingSection from "./layout/RatingSection";
 import TestimonySection from "./layout/TestimonySection";
-import Footer from "./layout/Footer";
 import "./App.css";
 
 function App() {
@@ -18,9 +13,7 @@ function App() {
       <Header/>
       <HeroSection/>
       <PetaSection/>
-      {/* <RatingSection/> */}
       <TestimonySection/>
-      <Footer/>
     </>
   );
 }
